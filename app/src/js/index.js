72b3010deddefb5e7097f485eb4339e8222bbd4c
Jesse <jessec.cameron@gmail.com>
2017-08-13 22:26:18 +1000

    more diamond magic

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -3,3 +3,23 @@
 require('../styles/imports.scss');
 
 console.log('hello world');
+
+const elements = document.getElementsByClassName('image-column');
+
+
+for(let i = 0; i < elements.length; i++) {
+    const trElement = document.createElement('div');
+    trElement.className += "tr";
+    const tlElement = document.createElement('div');
+    tlElement.className += "tl";
+    const brElement = document.createElement('div');
+    brElement.className += "br";
+    const blElement = document.createElement('div');
+    blElement.className += "bl";
+    const element = elements[i];
+    console.log(element);
+    element.appendChild(trElement);
+    element.appendChild(tlElement);
+    element.appendChild(brElement);
+    element.appendChild(blElement);
+}
