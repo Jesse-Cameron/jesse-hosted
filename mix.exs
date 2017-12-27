bc219a6add62c3b7e2f35c24a2d1e7488c784d43
Jesse <jessec.cameron@gmail.com>
2017-12-27 22:13:07 +1100

    added new dependencies

--- mix.exs
+++ mix.exs
@@ -37,7 +37,9 @@ defmodule JesseHosted.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:google_api_storage, "~> 0.0.1"},
+      {:goth, "~> 0.7.2"}
     ]
   end
 end
