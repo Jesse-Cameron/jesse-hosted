5b4b9dd471eb2a3a613aa35ba473e9a183b4152b
Jesse <jessec.cameron@gmail.com>
2018-01-03 23:05:22 +1100

    added working code and test cases

--- test/jesse_hosted_web/helpers/storage_helper_test.exs
+++ test/jesse_hosted_web/helpers/storage_helper_test.exs
@@ -1,11 +1,17 @@
 defmodule JesseHostedWeb.StorageHelperTest do
   use ExUnit.Case
 
+  alias JesseHostedWeb.GoogleStorageHelper, as: StorageHelper
+
   test "gets correct storage url" do
-    assert false
+    correct_path = "https://storage.googleapis.com/jesse-hosted-images/albums/album1/file1.jpg"
+    full_path = StorageHelper.image_url("album1", "file1.jpg")
+    assert correct_path == full_path
   end
 
-  test "get correct storage url - folder" do
-    assert false
+  test "get correct storage url - no folder" do
+    correct_path = "https://storage.googleapis.com/jesse-hosted-images/landing/file2.jpg"
+    full_path = StorageHelper.image_url("landing", "", "file2.jpg")
+    assert correct_path == full_path
   end
 end
