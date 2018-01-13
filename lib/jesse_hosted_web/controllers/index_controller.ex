d48f69bd279dbda26d95d119551f0a7b09b393b4
Jesse <jessec.cameron@gmail.com>
2018-01-13 23:36:59 +1100

    updated fn signature and added tests

--- lib/jesse_hosted_web/controllers/index_controller.ex
+++ lib/jesse_hosted_web/controllers/index_controller.ex
@@ -5,17 +5,17 @@ defmodule JesseHostedWeb.IndexController do
   import JesseHostedWeb.GoogleStorageHelper
 
   def index(conn, _params) do
-    index_map = read_json_to_map("site.json") |> to_album_map
+    index_map = read_json_to_map("site.json")["albums"] |> to_album_map
     render(conn, "index.html", index_map: index_map)
   end
 
-  defp to_album_map(index_map) do
-    index_map["albums"]
+  def to_album_map(index_map) do
+    index_map
     |> Enum.map(&update_fn &1)
   end
 
   defp update_fn(map) do
     album_id = map["album_number"] |> Integer.to_string()
-    Map.update!(map, "cover_image", &image_url(album_id, &1))
+    map |> Map.update!("cover_image", &image_url(album_id, &1))
   end
 end
