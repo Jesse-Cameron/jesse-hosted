542f4a912dbf411611e911318cdd4c93e5699476
Jesse <jessec.cameron@gmail.com>
2018-01-31 21:53:27 +1100

    removed gcb in favour of travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -27,3 +27,22 @@ docker push "$DOCKER_REPO:$TAG"
 
 docker tag "$SOURCE_CONTAINER" "$DOCKER_REPO:$TAG_LATEST"
 docker push "$DOCKER_REPO:$TAG_LATEST"
+
+# push to gcr
+
+# [START auth]
+# Decrypt the credentials we added to the repo using the key we added with the Travis command line tool
+openssl aes-256-cbc -K $encrypted_45d1b36fa803_key -iv $encrypted_45d1b36fa803_iv -in credentials.tar.gz.enc -out credentials.tar.gz -d
+# If the SDK is not already cached, download it and unpack it
+if [ ! -d ${HOME}/google-cloud-sdk ]; then
+    curl https://sdk.cloud.google.com | bash;
+fi
+tar -xzf credentials.tar.gz
+mkdir -p lib
+# Here we use the decrypted service account credentials to authenticate the command line tool
+gcloud auth activate-service-account --key-file client-secret.json
+# [END auth]
+
+# set the right project
+gcloud config set project jesse-hosted
+gcloud docker -- push us.gcr.io/jesse-host/"$DOCKER_REPO:$TAG_LATEST"
