b8e0e832ff49bae7c8665da45906c66fd6779ecd
Jesse <jessec.cameron@gmail.com>
2018-04-14 15:01:08 +1000

    readded docker build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,15 +12,17 @@ VERSION=${4}
 TAG=$(echo "${BRANCH//'/'/-}-${VERSION}")
 TAG_LATEST=$(echo "${BRANCH//'/'/-}-latest")
 
+docker-compose -f docker-compose.yml build
+
 # push to gcr
 
 # [START auth]
 # Decrypt the credentials we added to the repo using the key we added with the Travis command line tool
 # If the SDK is not already cached, download it and unpack it
-# if [ ! -d ${HOME}/google-cloud-sdk ]; then
-#     curl https://sdk.cloud.google.com | bash;
-# fi
-# tar -xzf credentials.tar.gz
+if [ ! -d ${HOME}/google-cloud-sdk ]; then
+    curl https://sdk.cloud.google.com | bash;
+fi
+tar -xzf credentials.tar.gz
 # Here we use the decrypted service account credentials to authenticate the command line tool
 gcloud auth activate-service-account --key-file jesse-hosted-key.json
 # [END auth]
