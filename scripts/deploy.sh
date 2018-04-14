1d0ea120fe7c08876c08b2e6edc21789ac9b12bb
Jesse <jessec.cameron@gmail.com>
2018-04-14 14:55:07 +1000

    debug command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,10 +17,10 @@ TAG_LATEST=$(echo "${BRANCH//'/'/-}-latest")
 # [START auth]
 # Decrypt the credentials we added to the repo using the key we added with the Travis command line tool
 # If the SDK is not already cached, download it and unpack it
-if [ ! -d ${HOME}/google-cloud-sdk ]; then
-    curl https://sdk.cloud.google.com | bash;
-fi
-tar -xzf credentials.tar.gz
+# if [ ! -d ${HOME}/google-cloud-sdk ]; then
+#     curl https://sdk.cloud.google.com | bash;
+# fi
+# tar -xzf credentials.tar.gz
 # Here we use the decrypted service account credentials to authenticate the command line tool
 gcloud auth activate-service-account --key-file jesse-hosted-key.json
 # [END auth]
@@ -28,4 +28,5 @@ gcloud auth activate-service-account --key-file jesse-hosted-key.json
 # set the right project
 gcloud config set project jesse-hosted
 docker tag "$SOURCE_CONTAINER" us.gcr.io/jesse-hosted/"$DOCKER_REPO:$TAG_LATEST"
+docker image ls
 gcloud docker -- push us.gcr.io/jesse-hosted/"$DOCKER_REPO:$TAG_LATEST"
