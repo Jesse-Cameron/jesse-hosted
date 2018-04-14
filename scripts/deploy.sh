40a0641a96c6657e8d363b584999621ccc55cdd6
Jesse <jessec.cameron@gmail.com>
2018-04-14 12:49:54 +1000

    removed non gcr deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,32 +2,6 @@
 
 echo "pushing container"
 
-#Check amount of arguments is correct
-if [ "$#" -ne 6 ]; then
-  usage
-  exit 1
-fi
-
-DOCKER_USER=${1}
-DOCKER_PASSWORD=${2}
-SOURCE_CONTAINER=${3}
-DOCKER_REPO=${4}
-BRANCH=${5}
-VERSION=${6}
-
-docker-compose -f docker-compose.yml build
-
-TAG=$(echo "${BRANCH//'/'/-}-${VERSION}")
-TAG_LATEST=$(echo "${BRANCH//'/'/-}-latest")
-
-docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
-
-docker tag "$SOURCE_CONTAINER" "$DOCKER_REPO:$TAG"
-docker push "$DOCKER_REPO:$TAG"
-
-docker tag "$SOURCE_CONTAINER" "$DOCKER_REPO:$TAG_LATEST"
-docker push "$DOCKER_REPO:$TAG_LATEST"
-
 # push to gcr
 
 # [START auth]
