3a00a40a687450cd21b32be2944c64a04358bea4
Jesse <jessec.cameron@gmail.com>
2018-04-26 10:37:09 +1000

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JesseHosted.Mixfile do
   def project do
     [
       app: :jesse_hosted,
-      version: "0.0.5",
+      version: "0.0.6",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
