d256dabea278f5c949fc5a4dbe321df355aeddb7
Jesse <jessec.cameron@gmail.com>
2018-05-04 20:42:34 +1000

    added gzip compression

--- lib/jesse_hosted_web/endpoint.ex
+++ lib/jesse_hosted_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule JesseHostedWeb.Endpoint do
     Plug.Static,
     at: "/",
     from: :jesse_hosted,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
   )
 
