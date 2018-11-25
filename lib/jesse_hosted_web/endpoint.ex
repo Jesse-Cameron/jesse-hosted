6b04188180d70e47872f953957211fe673bf89a0
Jesse <jessec.cameron@gmail.com>
2018-11-25 21:02:10 +1100

    upgraded pheonix to 1.4

--- lib/jesse_hosted_web/endpoint.ex
+++ lib/jesse_hosted_web/endpoint.ex
@@ -1,7 +1,8 @@
 defmodule JesseHostedWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :jesse_hosted
 
-  socket("/socket", JesseHostedWeb.UserSocket)
+  socket "/socket", JesseHostedWeb.UserSocket,
+    websocket: true
 
   # Serve at "/" the static files from "priv/static" directory.
   #
