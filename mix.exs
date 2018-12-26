a153f031cdde6ecd050ef50262b93c4b03d3cf7b
Jesse <jessec.cameron@gmail.com>
2018-12-26 20:57:22 +1100

    added wa album draft

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JesseHosted.Mixfile do
   def project do
     [
       app: :jesse_hosted,
-      version: "1.1.3",
+      version: "1.1.4",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
