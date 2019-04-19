cd47dbdafa20b644e66c366baef6ad11ae260c8f
Jesse <jessec.cameron@gmail.com>
2019-04-19 17:01:12 +1000

    feat: added scrset support to album pages

--- lib/jesse_hosted_web/views/album_view.ex
+++ lib/jesse_hosted_web/views/album_view.ex
@@ -1,3 +1,11 @@
 defmodule JesseHostedWeb.AlbumView do
   use JesseHostedWeb, :view
+
+  import JesseHostedWeb.GoogleStorageHelper
+
+  def get_srcset_filename(album_id, filename, size) do
+    [ name | ext ] = String.split(filename, ".")
+    ext_name = ".#{hd(ext)}"
+    image_url("albums/#{album_id}", "#{name}_#{size}#{ext_name}")
+  end
 end
